e147f9493729663c07a5fe1009e9f35827f2ce64
Ranvijay Singh <109961637+ranvijay18@users.noreply.github.com>
2023-12-02 19:12:21 +0530

    Update form.js

--- form.js
+++ form.js
@@ -1,8 +1,24 @@
+// var form = document.getElementById('form');
+// form.addEventListener('submit', store);
+
+// function store(e){
+//     e.preventDefault();
+//     localStorage.setItem('name', document.getElementById('name').value);
+//     localStorage.setItem('email', document.getElementById('email').value);
+// } 
+
 var form = document.getElementById('form');
 form.addEventListener('submit', store);
 
 function store(e){
     e.preventDefault();
-    localStorage.setItem('name', document.getElementById('name').value);
-    localStorage.setItem('email', document.getElementById('email').value);
-} 
+    var myobj = {
+        name : document.getElementById('name').value,
+        email : document.getElementById('email').value
+    }
+    
+    var myobj_serialized = JSON.stringify(myobj);
+
+    localStorage.setItem("myobj",myobj_serialized);
+
+}
